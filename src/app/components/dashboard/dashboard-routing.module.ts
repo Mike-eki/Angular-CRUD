db5afb3544b32c00a6bb07715f0510748ebd3a42
Miqueas <miqueas.moreno2016@gmail.com>
2022-11-26 08:10:57 -0300

    Added the details and edit components to the People table component, just basic, it needs styling and show and edit the data

--- src/app/components/dashboard/dashboard-routing.module.ts
+++ src/app/components/dashboard/dashboard-routing.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
 import { CreatePersonComponent } from './people/create-person/create-person.component';
+import { EditPersonComponent } from './people/edit-person/edit-person.component';
 import { PeopleComponent } from './people/people.component';
+import { PersonDetailsComponent } from './people/person-details/person-details.component';
 
 const routes: Routes = [
   { path: '', component: DashboardComponent, children: [
-    // { path: '', component: PeopleComponent },
     { path: 'people', component: PeopleComponent },
     { path: 'create-person', component: CreatePersonComponent },
+    { path: 'person-details/:id', component: PersonDetailsComponent},
+    { path: 'edit-person/:id', component: EditPersonComponent}
+    // { path: '**', component: PeopleComponent },
   ] }
 ];
 
--- src/app/components/dashboard/dashboard.module.ts
+++ src/app/components/dashboard/dashboard.module.ts
@@ -7,6 +7,8 @@ import { DashboardComponent } from './dashboard.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PeopleComponent } from './people/people.component';
 import { CreatePersonComponent } from './people/create-person/create-person.component';
+import { PersonDetailsComponent } from './people/person-details/person-details.component';
+import { EditPersonComponent } from './people/edit-person/edit-person.component';
 
 
 @NgModule({
@@ -14,7 +16,9 @@ import { CreatePersonComponent } from './people/create-person/create-person.comp
     DashboardComponent,
     NavbarComponent,
     PeopleComponent,
-    CreatePersonComponent
+    CreatePersonComponent,
+    PersonDetailsComponent,
+    EditPersonComponent
   ],
   imports: [
     CommonModule,
